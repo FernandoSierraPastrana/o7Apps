077e8a3c179b0aff28ad972f244e53f1acf6cd99
Fernando Sierra <fernando.sierra.pastrana@gmail.com>
2018-01-07 19:04:19 -0600

    List applications installed
    Add of more information to the detail view

--- app/src/main/java/com/o7/apps/O7Extensions.kt
+++ app/src/main/java/com/o7/apps/O7Extensions.kt
@@ -2,11 +2,19 @@ package com.o7.apps
 
 import android.content.Context
 import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun Context.getAppsInstalled(rootPackage: String? = null): List<ApplicationInfo> {
-    var installedApps = packageManager.getInstalledApplications(0)
+    var installedApps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
     if (rootPackage != null) {
         installedApps = installedApps.filter { it.packageName.startsWith(rootPackage) }
     }
     return installedApps
+}
+
+fun Long.format(): String {
+    val formatter = SimpleDateFormat("dd MMMM yyyy hh:mm aaa", Locale.getDefault())
+    return formatter.format(Date(this))
 }
--- app/src/main/java/com/o7/apps/mvp/view/DetailView.kt
+++ app/src/main/java/com/o7/apps/mvp/view/DetailView.kt
@@ -2,10 +2,12 @@ package com.o7.apps.mvp.view
 
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import android.os.Build
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.o7.apps.R
+import com.o7.apps.format
 import com.o7.apps.mvp.fragment.AppDetailFragment
 import java.lang.ref.WeakReference
 
@@ -33,10 +35,15 @@ class DetailView(fragment: AppDetailFragment) {
     }
 
     private fun buildOtherInfoString(fragment: AppDetailFragment, packageManager: PackageManager, appInfo: ApplicationInfo): String {
-        val packageInfo = packageManager.getPackageInfo(appInfo.packageName, 0)
+        val packageInfo = packageManager.getPackageInfo(appInfo.packageName, PackageManager.GET_PERMISSIONS)
         val stringBuilder = StringBuilder()
         stringBuilder.append(fragment.getString(R.string.version_code_template).format(packageInfo.versionCode))
         stringBuilder.append(fragment.getString(R.string.version_name_template).format(packageInfo.versionName))
+        stringBuilder.append(fragment.getString(R.string.last_update_template).format(packageInfo.lastUpdateTime.format()))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            stringBuilder.append(fragment.getString(R.string.min_sdk_template).format(appInfo.minSdkVersion))
+        }
+        stringBuilder.append(fragment.getString(R.string.target_sdk_template).format(appInfo.targetSdkVersion))
         return stringBuilder.toString()
     }
 }
